7c62264911d68b8093e1f40c564882e3bd494bb6
jiahui-97 <jhzhang97@163.com>
2022-05-17 22:23:57 +0800

    feat(proj1): 初始化并实现 LRU

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # GeeCache
 一个分布式缓存系统
 
+- [x] LRU 缓存淘汰策略
+- [ ] 单机并发缓存
+- [ ] HTTP 服务端
+- [ ] 一致性哈希
+- [ ] 分布式节点
+- [ ] 防止缓存击穿
+- [ ] 使用 Protobuf 通信
+
 参考：https://geektutu.com/post/geecache.html
---
+++ lru/lru.go
@@ -0,0 +1,73 @@
+package lru
+
+import "container/list"
+
+type Cache struct {
+	maxBytes int64
+	nbytes   int64
+	deque    *list.List
+	cache    map[string]*list.Element
+	// 元素被移除时的回调函数，可以为空
+	OnEvicted func(key string, value Value)
+}
+
+type entry struct {
+	key   string
+	value Value
+}
+
+type Value interface {
+	Len() int
+}
+
+func New(maxBytes int64, onEvicted func(string, Value)) *Cache {
+	return &Cache{
+		maxBytes:  maxBytes,
+		nbytes:    0,
+		deque:     list.New(),
+		cache:     make(map[string]*list.Element),
+		OnEvicted: onEvicted,
+	}
+}
+
+func (c *Cache) Get(key string) (value Value, ok bool) {
+	if ele, ok := c.cache[key]; ok {
+		c.deque.MoveToFront(ele)
+		kv := ele.Value.(*entry)
+		return kv.value, true
+	}
+	return
+}
+
+func (c *Cache) RemoveOldest() {
+	ele := c.deque.Back()
+	if ele != nil {
+		c.deque.Remove(ele)
+		kv := ele.Value.(*entry)
+		delete(c.cache, kv.key)
+		c.nbytes -= int64(len(kv.key)) + int64(kv.value.Len())
+		if c.OnEvicted != nil {
+			c.OnEvicted(kv.key, kv.value)
+		}
+	}
+}
+
+func (c *Cache) Add(key string, value Value) {
+	if ele, ok := c.cache[key]; ok {
+		c.deque.MoveToFront(ele)
+		kv := ele.Value.(*entry)
+		c.nbytes += int64(value.Len()) - int64(kv.value.Len())
+		kv.value = value
+	} else {
+		ele := c.deque.PushFront(&entry{key, value})
+		c.cache[key] = ele
+		c.nbytes += int64(len(key)) + int64(value.Len())
+	}
+	for c.maxBytes != 0 && c.maxBytes < c.nbytes {
+		c.RemoveOldest()
+	}
+}
+
+func (c *Cache) Len() int {
+	return c.deque.Len()
+}
